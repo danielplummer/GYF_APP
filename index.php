d74bcfdd96002a231ad2a62039dbef602d6f031a
Dan Plummer <danplummer92@outlook.com>
2021-03-04 21:06:02 -0600

    Footer
    Added footer to includes file

--- index.php
+++ index.php
@@ -73,29 +73,5 @@
     </div>
 </section>
 
-
-  <!-- Footer -->
-  <footer class="py-5 bg-grey">
-    <div class="container text-center">
-      <p>Copyright &copy; Grow Young Fitness 2021</p>
-        <ul class="nav justify-content-center">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Privacy Policy</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Terms of Service</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Admin Login</a>
-          </li>
-        </ul>
-    </div>
-    <!-- /.container -->
-  </footer>
-
-
-    <!-- jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-  </body>
-</html>
+<!-- Footer -->
+<?php include "includes/footer.php" ?>
