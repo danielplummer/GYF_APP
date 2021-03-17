02dbf145eea548c31093729a071e8e9d2d4f1a84
Dan Plummer <danplummer92@outlook.com>
2021-03-16 22:32:45 -0500

    Admin Widgets
    Displaying dynamic data counts for widgets

--- admin/index.php
+++ admin/index.php
@@ -6,53 +6,77 @@
 
   	
 <!-- Main Admin Page Content -->
-<div class="container-fluid">
-	<h1 class="my-5">Main Dashboard</h1>
-	
-	<div class="row">
-	    <!-- Posts -->
-	    <div class="col-md-4">
-	        <div class="card bg-primary text-white mb-4">
-	            <div class="card-body d-flex align-items-center justify-content-between">
-	            	<h4><i class="fas fa-sticky-note"></i> Posts</h4>
-	            	<h4>74</h4>
-	        	</div>
-	            <div class="card-footer d-flex align-items-center justify-content-between">
-	                <a class="small text-white stretched-link" href="#">View Details</a>
-	                <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-	            </div>
-	        </div>
-	    </div>
-	    <!-- Comments -->
-	    <div class="col-md-4">
-	        <div class="card bg-success text-white mb-4">
-	            <div class="card-body d-flex align-items-center justify-content-between">
-	            	<h4><i class="fas fa-comment-dots"></i> Comments</h4>
-	            	<h4>74</h4>
-	        	</div>
-	            <div class="card-footer d-flex align-items-center justify-content-between">
-	                <a class="small text-white stretched-link" href="#">View Details</a>
-	                <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-	            </div>
-	        </div>
-	    </div>
-	    <!-- users -->
-	    <div class="col-md-4">
-	        <div class="card bg-danger text-white mb-4">
-	            <div class="card-body d-flex align-items-center justify-content-between">
-	            	<h4><i class="fas fa-user"></i> Users</h4>
-	            	<h4>74</h4>
-	        	</div>
-	            <div class="card-footer d-flex align-items-center justify-content-between">
-	                <a class="small text-white stretched-link" href="#">View Details</a>
-	                <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-	            </div>
-	        </div>
-	    </div>
+<section id="widgets">
+	<div class="container-fluid">
+		<h1 class="my-5">Main Dashboard</h1>
 
+		<div class="row">
+		    
+		    <!-- Posts -->
+		    <?php
+		    // Get number of posts from db
+		    $query = "SELECT * FROM posts";
+		    $select_all_posts = mysqli_query($connection, $query);
+		    $posts_count = mysqli_num_rows($select_all_posts)
+		    ?>
+
+		    <div class="col-md-4">
+		        <div class="card bg-primary text-white mb-4">
+		            <div class="card-body d-flex align-items-center justify-content-between">
+		            	<h4><i class="fas fa-sticky-note"></i> Posts</h4>
+		            	<h4><?php echo $posts_count ?></h4>
+		        	</div>
+		            <div class="card-footer d-flex align-items-center justify-content-between">
+		                <a class="small text-white stretched-link" href="posts.php">View All Posts</a>
+		                <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+		            </div>
+		        </div>
+		    </div>
+		    
+		    <!-- Comments -->
+		    <?php
+		    // Get number of all comments from db
+		    $query = "SELECT * FROM comments";
+		    $select_all_comments = mysqli_query($connection, $query);
+		    $comment_count = mysqli_num_rows($select_all_comments)
+		    ?>
+		    <div class="col-md-4">
+		        <div class="card bg-success text-white mb-4">
+		            <div class="card-body d-flex align-items-center justify-content-between">
+		            	<h4><i class="fas fa-comment-dots"></i> Comments</h4>
+		            	<h4><?php echo $comment_count ?></h4>
+		        	</div>
+		            <div class="card-footer d-flex align-items-center justify-content-between">
+		                <a class="small text-white stretched-link" href="comments.php">View All Comments</a>
+		                <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+		            </div>
+		        </div>
+		    </div>
+		    
+		    <!-- users -->
+		    <?php
+		    // Get number of all users from db
+		    $query = "SELECT * FROM users";
+		    $select_all_users = mysqli_query($connection, $query);
+		    $user_count = mysqli_num_rows($select_all_users)
+		    ?>
+		    <div class="col-md-4">
+		        <div class="card bg-danger text-white mb-4">
+		            <div class="card-body d-flex align-items-center justify-content-between">
+		            	<h4><i class="fas fa-user"></i> Users</h4>
+		            	<h4><?php echo $user_count ?></h4>
+		        	</div>
+		            <div class="card-footer d-flex align-items-center justify-content-between">
+		                <a class="small text-white stretched-link" href="users.php">View All Users</a>
+		                <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+		            </div>
+		        </div>
+		    </div>
+
+		</div>
+	<!--/container -->
 	</div>
-<!--/container -->
-</div>
+</section>
 
 
 
