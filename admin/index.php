5f77d12ef2474a8ed112c50db1e5097449cec90a
Dan Plummer <danplummer92@outlook.com>
2021-03-16 21:27:10 -0500

    Admin Dashboard Widgets
    set up some basic admin dashboard  widgets

--- admin/index.php
+++ admin/index.php
@@ -7,9 +7,56 @@
   	
 <!-- Main Admin Page Content -->
 <div class="container-fluid">
-	<h1 class="mt-4">Main Dashboard</h1>
-	<p> welcome <?php echo $_SESSION['username'] ?></p>
+	<h1 class="my-5">Main Dashboard</h1>
+	
+	<div class="row">
+	    <!-- Posts -->
+	    <div class="col-md-4">
+	        <div class="card bg-primary text-white mb-4">
+	            <div class="card-body d-flex align-items-center justify-content-between">
+	            	<h4><i class="fas fa-sticky-note"></i> Posts</h4>
+	            	<h4>74</h4>
+	        	</div>
+	            <div class="card-footer d-flex align-items-center justify-content-between">
+	                <a class="small text-white stretched-link" href="#">View Details</a>
+	                <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+	            </div>
+	        </div>
+	    </div>
+	    <!-- Comments -->
+	    <div class="col-md-4">
+	        <div class="card bg-success text-white mb-4">
+	            <div class="card-body d-flex align-items-center justify-content-between">
+	            	<h4><i class="fas fa-comment-dots"></i> Comments</h4>
+	            	<h4>74</h4>
+	        	</div>
+	            <div class="card-footer d-flex align-items-center justify-content-between">
+	                <a class="small text-white stretched-link" href="#">View Details</a>
+	                <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+	            </div>
+	        </div>
+	    </div>
+	    <!-- users -->
+	    <div class="col-md-4">
+	        <div class="card bg-danger text-white mb-4">
+	            <div class="card-body d-flex align-items-center justify-content-between">
+	            	<h4><i class="fas fa-user"></i> Users</h4>
+	            	<h4>74</h4>
+	        	</div>
+	            <div class="card-footer d-flex align-items-center justify-content-between">
+	                <a class="small text-white stretched-link" href="#">View Details</a>
+	                <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+	            </div>
+	        </div>
+	    </div>
+
+	</div>
+<!--/container -->
 </div>
-    
+
+
+
+
+
 <!-- Footer -->
 <?php include "includes/footer.php"?>
