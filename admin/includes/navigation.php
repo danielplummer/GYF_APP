f35b21ef2c3a387928c0f14efc95e763fcbd0ba9
Dan Plummer <danplummer92@outlook.com>
2021-03-25 13:00:23 -0500

    Nav Style
    updated Navigation

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -6,7 +6,6 @@
       <div class="sidebar-heading"><img src="img/lg-logo-white.png" class="img-fluid" width="175"></div>
       <div class="list-group list-group-flush">
         <a href="index.php" class="list-group-item list-group-item-action bg-dark text-light"><i class="fas fa-tachometer-alt mr-3"></i>Dashboard</a>
-        <!--<a href="posts.php" class="list-group-item list-group-item-action bg-dark text-light"><i class="fas fa-sticky-note mr-3"></i> Posts</a>-->
         <!-- Posts Dropdown -->
         <a href="#" data-toggle="collapse" data-target="#posts" class="collapsed list-group-item list-group-item-action bg-dark text-light">
           <i class="fas fa-sticky-note mr-3"></i> Posts<i class="fas fa-chevron-down float-right mt-1"></i>
@@ -15,7 +14,6 @@
           <li class="py-2"><a href="posts.php" class="text-light">View All Posts</a></li>
           <li class="py-2"><a href="pending-posts.php" class="text-light">View Pending Posts</a></li>
         </ul>
-        <!--<a href="comments.php" class="list-group-item list-group-item-action bg-dark text-light"><i class="fas fa-comment-dots mr-3"></i>Comments</a>-->
         <!-- Comments Dropdown -->
         <a href="#" data-toggle="collapse" data-target="#comments" class="collapsed list-group-item list-group-item-action bg-dark text-light">
           <i class="fas fa-comment-dots mr-3"></i>Comments<i class="fas fa-chevron-down float-right mt-1"></i>
@@ -38,12 +36,15 @@
       <!-- Top Navigation -->
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom">
         <!-- sidebar toggle -->
-        <button class="btn btn-light" id="menu-toggle"><i class="fas fa-ellipsis-h"></i></button>
+        <button class="btn btn-light" id="menu-toggle"><i class="fas fa-bars"></i></button>
 
+        <!--
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+      -->
 
+        <!--
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
             <li class="nav-item">
@@ -55,12 +56,30 @@
                 <?php echo $_SESSION['username'] ?>
               </a>
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="../index.php">Back to App</a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="includes/logout.php"><i class="fas fa-power-off"></i> Logout</a>
               </div>
             </li>
           </ul>
         </div>
+      -->
+
+
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user-cog"></i>
+                <?php echo $_SESSION['username'] ?>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="../index.php">Back to App</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="includes/logout.php"><i class="fas fa-power-off"></i> Logout</a>
+              </div>
+            </li>
+          </ul>
+
+
+
       </nav>
